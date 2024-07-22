d0dd428ac4b1ae3c65d362440c3a1452fae43beb
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-07-22 15:36:42 -0300

    refactor: move application.module to its own /application folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ApplicationModule } from './external/application.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), ApplicationModule],
