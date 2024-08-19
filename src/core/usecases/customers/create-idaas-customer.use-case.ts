d32e0f5db1ebc3475794121d77edb02d0a47c6d4
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-08-19 18:47:35 -0300

    refactor: remove dependencies not required and update api gateway interface methods

--- src/core/usecases/customers/create-idaas-customer.use-case.ts
+++ src/core/usecases/customers/create-idaas-customer.use-case.ts
@@ -2,13 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { Customer } from '../../entities/customer.entity';
 import { IUseCase } from '../usecase';
 import { ApiGatewayService } from '../../../external/integrations/api-gateway-service';
-import { GetIdaasCustomerByEmailUseCase } from '../../usecases/customers/get-idaas-customer-by-email.use-case'
 
 @Injectable()
 export class CreateIdaasCustomerUseCase implements IUseCase<Customer> {
   constructor(
     private apiGateway: ApiGatewayService,
-    private getIdaasCustomerByEmailUseCase: GetIdaasCustomerByEmailUseCase,
   ) {}
 
   async execute(customer: Customer): Promise<any> {
